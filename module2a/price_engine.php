a8d7310bb30af475b6b7a387ee8b022121997a8e
Athina Cicchetti <cicchetti_athina_den@student.smc.edu>
2025-06-29 17:49:59 -0700

    Completed refactored price_engine_refactored.php

--- module2a/price_engine.php
+++ module2a/price_engine.php
@@ -17,7 +17,7 @@
         <h1>Order Summary</h1>
         <?php
             // --- Configuration: Change these values to test all business rules! ---
-            $size = 'XL'; // Options: 'S', 'M', 'L', 'XL'
+            $size = 'S'; // Options: 'S', 'M', 'L', 'XL'
             $color = 'Sunset Orange'; // Any string, but test with 'Sunset Orange' or 'Ocean Blue'
             $isCustomized = true; // Options: true, false
             $customerFirstName = 'Athina'; // <-- IMPORTANT: REPLACE WITH YOUR ACTUAL FIRST NAME
